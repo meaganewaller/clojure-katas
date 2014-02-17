0e01855d597eaeaa4659dd2224d9b20534a2f294
Meagan Waller <meaganewaller@gmail.com>
2014-02-17 16:23:05 -0600

    refactoring

--- fizzbuzz/src/fizzbuzz/core.clj
+++ fizzbuzz/src/fizzbuzz/core.clj
@@ -1,7 +1,15 @@
 (ns fizzbuzz.core)
+(defn fizz? [number]
+  (zero? (rem number 3)))
+
+(defn buzz? [number]
+  (zero? (rem number 5)))
+
+(defn fizz-buzz? [number]
+  (and (fizz? number) (buzz? number)))
+
 (defn fizzbuzz [number]
-  (if (zero?(rem number 15)) "fizzbuzz"
-  (if (zero? (rem number 5)) "buzz"
-    (if (zero? (rem number 3)) "fizz" number
-  )))
-)
+  (if (fizz-buzz? number) "fizzbuzz"
+    (if (fizz? number) "fizz"
+      (if (buzz? number) "buzz" number))))
+
